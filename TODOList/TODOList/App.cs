78e48b7e16102d9a63649dfb3b7e996d1e2951ae
Luis Fernando Camacho <luis.fernando.c.c.1987@gmail.com>
2017-04-07 14:14:13 -0700

    folder structure

---
+++ TODOList/TODOList/App.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TODOList.Views;
+using Xamarin.Forms;
+
+namespace TODOList
+{
+    public class App : Application
+    {
+        public App()
+        {
+            //var navigationPage = new NavigationPage(new MainP());
+            //navigationPage.BarBackgroundColor = Color.Red;
+            //MainPage = navigationPage;
+            MainPage = new MainP();
+            
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ TODOList/TODOList/MainP.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace TODOList
+{
+    public partial class MainP : MasterDetailPage
+    {
+        public MainP()
+        {
+            InitializeComponent();
+        }
+    }
+}
